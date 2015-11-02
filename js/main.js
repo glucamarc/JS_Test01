9b630e3412da5144ebebc100e415612641dc3882
glucamarc <gianluca.m@gi-mar.it>
2015-11-02 21:13:12 +0100

    Aggiunte prove lodash

--- js/main.js
+++ js/main.js
@@ -11,10 +11,54 @@ $(document).ready(function(event) {
 
 
 function testA() {
-    console.log($("#field1").val());
-    var defer = Q.defer();
-    var v = $("#field1").val("bbb");
+//    var arr = [10,1,20,1.2,10,100,1];
+//    console.log(_.uniq(arr,true));
+    
+    
+    var arr =[];
+    var data1 = {
+        nome:"a",
+        eta:10
+    }
+    var data2 = {
+        nome:"b",
+        eta:20
+    }
+    var data3 = {
+        nome:"c",
+        eta:30
+    }
+    arr.push(data1,data2,data3);
+    console.log(arr);
+    
+    
+    var val = _.result(_.find(arr, function(chr){
+        return chr.eta == 20;
+    }), 'nome');
+    console.log(val);
+    
+    
+    var objVal = _.find(arr, function(items){
+        return items.nome == 'b';
+    });
+    console.log(objVal); // Restituisce l'oggetto trovato per key
+    
+
+    console.log(_.pluck(arr,'nome')); // Restituisce i valori di nome
+
+    
+    //var indexVal = _.findIndex(arr, { nome: 'b' }); // Equivalente alla sintassi sotto
+    var indexVal = _.findIndex(arr, function(chr){
+        return chr.nome === 'b';
+    });
+    console.log(indexVal);
+    
+    arr.splice(indexVal,1);
+    console.log(arr);
+
+    
 }
+
 
 
 
